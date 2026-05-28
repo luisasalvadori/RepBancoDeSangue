e3c31ce867859ea9b6458eb197a331fab0ff94ce
mclarar-coura <maria.coura@geb.inatel.br>
2026-05-27 22:08:19 -0300

    Add files via upload

---
+++ src/br/inatel/bancodesangue/Menu/Menu.java
@@ -0,0 +1,4 @@
+package br.inatel.bancodesangue.Menu;
+
+public class Menu {
+}
---
+++ src/br/inatel/bancodesangue/Sangue/Compatibilidade/Compativel.java
@@ -0,0 +1,7 @@
+package br.inatel.bancodesangue.Sangue.Compatibilidade;
+
+public class Compativel {
+    public boolean verificarCompatibilidade(){
+
+    }
+}
