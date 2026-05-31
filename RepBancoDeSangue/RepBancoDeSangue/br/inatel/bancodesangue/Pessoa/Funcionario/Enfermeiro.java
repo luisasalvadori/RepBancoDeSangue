e44f277c05c5b32c92e0f52cd771822a3f6ca5a9
Anny <annygabrielefrancisco52@gmail.com>
2026-05-31 18:10:47 -0300

    Pessoa, Doador, Paciente, Funcionario, Administrador, Enfermeiro, Doação

---
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Funcionario/Enfermeiro.java
@@ -0,0 +1,42 @@
+package br.inatel.bancodesangue.Pessoa.Funcionario;
+
+import br.inatel.bancodesangue.Pessoa.Doador.Doador;
+import br.inatel.bancodesangue.Sangue.BolsaSangue;
+import br.inatel.bancodesangue.Sangue.Doacao.Doacao;
+
+import java.time.LocalDate;
+
+public class Enfermeiro extends Funcionario{
+    protected String corem;
+
+    public Enfermeiro(String cargo, String matricula, String turno, int qtdatendimentos, String corem) {
+        super(nome,idade, cpf,cargo, matricula, turno, qtdatendimentos);
+        this.corem = corem;
+    }
+
+    public void cadastrarDoador(){
+        System.out.println("doador cadastrado com sucesso");
+    }
+
+    public boolean entrevistarDoador(Doador doador){
+        return doador.validarDados();
+    }
+
+    public Doacao coletarSangue(Doador doador, BolsaSangue bolsa){
+        if(!entrevistarDoador(doador))
+        {
+            System.out.println("Doador nao apto para doacao");
+            return null;
+        }
+
+        return new Doacao(LocalDate.now(),doador,this,bolsa);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Enfermeiro{" +
+                "corem='" + corem + '\'' +
+                '}';
+    }
+}
---
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Sangue/Doacao/Doacao.java
@@ -0,0 +1,40 @@
+package br.inatel.bancodesangue.Sangue.Doacao;
+
+import br.inatel.bancodesangue.Pessoa.Doador.Doador;
+import br.inatel.bancodesangue.Pessoa.Funcionario.Enfermeiro;
+import br.inatel.bancodesangue.Sangue.BolsaSangue;
+
+import java.time.LocalDate;
+
+public class Doacao {
+    private LocalDate datacoleta;
+    private Doador doador;
+    private Enfermeiro enfermeiro;
+    private BolsaSangue bolsa;
+
+    public Doacao(LocalDate datacoleta, Doador doador, Enfermeiro enfermeiro, BolsaSangue bolsa) {
+        this.datacoleta = datacoleta;
+        this.doador = doador;
+        this.enfermeiro = enfermeiro;
+        this.bolsa = bolsa;
+    }
+
+    public BolsaSangue gerarBolsa(){
+        return bolsa;
+    }
+
+    public void registrarDoacao(){
+        System.out.println("Doacao registrada em: " + datacoleta);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Doacao{" +
+                "datacoleta=" + datacoleta +
+                ", doador=" + doador +
+                ", enfermeiro=" + enfermeiro +
+                ", bolsa=" + bolsa +
+                '}';
+    }
+}
