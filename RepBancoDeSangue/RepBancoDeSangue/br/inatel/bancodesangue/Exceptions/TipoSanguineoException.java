3e3f322d6fbba9744edfa3b3a29ed38ac822b997
mclarar-coura <maria.coura@geb.inatel.br>
2026-06-01 11:45:19 -0300

    Add files via upload

---
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Exceptions/TipoSanguineoException.java
@@ -0,0 +1,7 @@
+package br.inatel.bancodesangue.Exceptions;
+
+public class TipoSanguineoException extends Exception{
+    public TipoSanguineoException (String mensagem){
+        super(mensagem);
+    }
+}
