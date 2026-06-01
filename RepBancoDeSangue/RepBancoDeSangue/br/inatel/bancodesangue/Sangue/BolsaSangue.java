b634ec493757a2e09ae00a52e4919939416cdced
mclarar-coura <maria.coura@geb.inatel.br>
2026-06-01 11:42:50 -0300

    Update BolsaSangue.java

--- RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Sangue/BolsaSangue.java
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Sangue/BolsaSangue.java
@@ -1,4 +1,5 @@
 package br.inatel.bancodesangue.Sangue;
+import br.inatel.bancodesangue.Exceptions.TipoSanguineoException;
 
 import java.time.LocalDate;
 
@@ -6,14 +7,34 @@ public class BolsaSangue {
     private String tipoS;
     private LocalDate datacoleta;
     private LocalDate datavalidade;
+    private String []tiposValidos = {"O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"};
 
-    public BolsaSangue(String tipoS, LocalDate datacoleta, LocalDate datavalidade) {
-        this.tipoS = tipoS;
+
+    public BolsaSangue(String tipoS, LocalDate datacoleta, LocalDate datavalidade) throws TipoSanguineoException {
+        boolean validado = false;
+        for (String tipoValido : tiposValidos){
+            if (tipoValido.equalsIgnoreCase(tipoS)){
+                this.tipoS = tipoS;
+                validado = true;
+            }
+        }
+        if (!validado){
+            throw new TipoSanguineoException("Tipo sanguíneo inexistente");
+        }
+
         this.datacoleta = datacoleta;
         this.datavalidade = datavalidade;
     }
-
-    public boolean verificarValidade(){
-
+    public boolean verificarValidade(LocalDate data) { //data do uso da bolsa
+        return data.isBefore(datavalidade); //true = dentro da validade; false = vencida
+    }
+    public String getTipoS(){
+        return tipoS;
+    }
+    public LocalDate getDataColeta(){
+        return datacoleta;
+    }
+    public LocalDate getDataValidade() {
+        return datavalidade;
     }
 }
