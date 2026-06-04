7b8002a9aeacd123abf0a6246e30b68ce5c9ed7c
Anny <annygabrielefrancisco52@gmail.com>
2026-06-04 11:40:32 -0300

    Refactor Enfermeiro class with new methods and validation
    Updated the Enfermeiro class to include exception handling, improved methods for registering donors, and enhanced data validation.

--- RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Funcionario/Enfermeiro.java
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Funcionario/Enfermeiro.java
@@ -3,40 +3,58 @@ package br.inatel.bancodesangue.Pessoa.Funcionario;
 import br.inatel.bancodesangue.Pessoa.Doador.Doador;
 import br.inatel.bancodesangue.Sangue.BolsaSangue;
 import br.inatel.bancodesangue.Sangue.Doacao.Doacao;
-
+import br.inatel.bancodesangue.Exceptions.DadosInvalidosException;
 import java.time.LocalDate;
 
 public class Enfermeiro extends Funcionario{
-    protected String corem;
+    private String corem;
 
     public Enfermeiro(String nome, int idade, String cpf, String sexo, String cargo, String matricula, String corem) {
         super(nome, idade, cpf, sexo, cargo, matricula);
         this.corem = corem;
     }
 
-    public void cadastrarDoador(){
-        System.out.println("doador cadastrado com sucesso");
+    public void cadastrarDoador(Doador doador){
+        System.out.println("Doador " + doador.getnome() + " cadastrado com sucesso");
     }
 
     public boolean entrevistarDoador(Doador doador){
-        return doador.validarDados();
+        boolean apto = doador.validarDados();
+        if(apto == true)
+        {
+            System.out.println("Doador apto para doacao")
+        }else{
+            System.out.println("Doador nao apto para doacao");
+        }
+        return apto;
     }
 
-    public Doacao coletarSangue(Doador doador, BolsaSangue bolsa){
+    public Doacao coletarSangue(Doador doador, BolsaSangue bolsa) throws DadosInvalidadosException {
+        return coletarSangue(doador, bolsa, LocalDate.now());
+    }
+
+    public Doacao coletarSangue(Doador doador, BolsaSangue bolsa, LocalDate dataColeta) throws DadosInvalidadosException {
+        
         if(!entrevistarDoador(doador))
         {
-            System.out.println("Doador nao apto para doacao");
             return null;
         }
-
-        return new Doacao(LocalDate.now(),doador,this,bolsa);
-
+        doador.registrarDoacao(dataColeta);
+        return new Doacao(dataColeta, doador, this, bolsa);
     }
 
-    @Override
-    public String toString() {
-        return "Enfermeiro{" +
-                "corem='" + corem + '\'' +
-                '}';
+        @Override
+        public boolean validarDados() { return super.validarDados() && coren != null && !coren.isBlank(); }
+
+        @Overrride
+        public void mostrarDados() {
+            System.out.print("===== ENFERMEIRO =====");
+            super.mostrarDados();
+            System.out.println("COREN: " + coren);
+        }
     }
-}
+    
+
+
+    
+
