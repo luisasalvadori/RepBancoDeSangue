daa18a8fc437a4bfc388b870066b8d49993b1e88
mclarar-coura <maria.coura@geb.inatel.br>
2026-06-04 12:17:07 -0300

    Update BolsaSangue.java

--- RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Sangue/BolsaSangue.java
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Sangue/BolsaSangue.java
@@ -1,40 +1,50 @@
 package br.inatel.bancodesangue.Sangue;
-import br.inatel.bancodesangue.Exceptions.TipoSanguineoException;
 
+import br.inatel.bancodesangue.Exceptions.DadosInvalidosException;
+import br.inatel.bancodesangue.Exceptions.TipoSanguineoException;
+import br.inatel.bancodesangue.Util.DataUtil;
+import br.inatel.bancodesangue.Util.ValidadorDados;
 import java.time.LocalDate;
 
 public class BolsaSangue {
+    private static int proximoId = 1;
+
+    private int idBolsa;
     private String tipoS;
-    private LocalDate datacoleta;
-    private LocalDate datavalidade;
-    private String []tiposValidos = {"O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"};
+    private LocalDate dataColeta;
+    private LocalDate dataValidade;
+    private boolean liberada;
 
+    public BolsaSangue(String tipoS, LocalDate dataColeta) throws TipoSanguineoException, DadosInvalidosException {
+        ValidadorDados.validarTipoSanguineo(tipoS);
+        ValidadorDados.validarDataNaoFutura(dataColeta, "Data de coleta da bolsa");
+        this.idBolsa = proximoId++;
+        this.tipoS = tipoS.trim().toUpperCase();
+        this.dataColeta = dataColeta;
+        this.dataValidade = dataColeta.plusDays(35);
+        this.liberada = false;
+    }
 
-    public BolsaSangue(String tipoS, LocalDate datacoleta, LocalDate datavalidade) throws TipoSanguineoException {
-        boolean validado = false;
-        for (String tipoValido : tiposValidos){
-            if (tipoValido.equalsIgnoreCase(tipoS)){
-                this.tipoS = tipoS;
-                validado = true;
-            }
-        }
-        if (!validado){
-            throw new TipoSanguineoException("Tipo sanguíneo inexistente");
-        }
+    public boolean verificarValidade(LocalDate dataUso) {
+        return !dataUso.isAfter(dataValidade);
+    }
 
-        this.datacoleta = datacoleta;
-        this.datavalidade = datavalidade;
+    public void liberar() { this.liberada = true; }
+
+    public void mostrarDados() {
+        System.out.println("ID: " + idBolsa + " | Tipo: " + tipoS + " | Coleta: " + DataUtil.formatar(dataColeta) + " | Validade: " + DataUtil.formatar(dataValidade) + " | Liberada: " + (liberada ? "sim" : "não"));
     }
-    public boolean verificarValidade(LocalDate data) { //data do uso da bolsa
-        return !data.isAfter(datavalidade); //true = dentro da validade; false = vencida, a bolsa está válida se a data de uso não for depois da data de validade.
+
+    @Override
+    public String toString() {
+        return idBolsa + ";" + tipoS + ";" + DataUtil.formatar(dataColeta) + ";" + DataUtil.formatar(dataValidade) + ";" + liberada;
     }
-    public String getTipoS(){
-        return tipoS;
-    }
-    public LocalDate getDataColeta(){
-        return datacoleta;
-    }
-    public LocalDate getDataValidade() {
-        return datavalidade;
-    }
+
+    public static String[] getTiposValidos() { return ValidadorDados.getTiposSanguineosValidos(); }
+    public int getIdBolsa() { return idBolsa; }
+    public String getTipoS() { return tipoS; }
+    public LocalDate getDataColeta() { return dataColeta; }
+    public LocalDate getDataValidade() { return dataValidade; }
+    public boolean isLiberada() { return liberada; }
 }
+    
