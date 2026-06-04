39fd6a978847458fe7eb202d7848874a5d3a9d8c
Maria Luisa Barroso <marialuisabarroso1908@hotmail.com>
2026-06-04 11:02:43 -0300

    Update Administrador.java

--- RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Funcionario/Administrador.java
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Funcionario/Administrador.java
@@ -1,38 +1,26 @@
 package br.inatel.bancodesangue.Pessoa.Funcionario;
 
+import br.inatel.bancodesangue.Banco.BancoSangue;
+
 public class Administrador extends Funcionario {
     public Administrador(String nome, int idade, String cpf, String sexo, String cargo, String matricula) {
         super(nome, idade, cpf, sexo, cargo, matricula);
     }
 
-    public void gerarRelatorioGeral() {
-        System.out.println("Relatório geral gerado pelo administrador " + nome);
+    public void gerarRelatorioGeral(BancoSangue banco) {
+        System.out.println("Relatório geral gerado pelo administrador " + getNome());
+        banco.listarEstoque();
     }
 
-    public boolean verificarEstoque() {
-        System.out.println("Administrador " + nome + " verificando o estoque.");
-        return true;
+    public boolean verificarEstoque(BancoSangue banco) {
+        boolean temEstoque = banco.getTotalBolsas > 0;
+        System.out.println("Estoque possui" +  banco.getTotalBolsas() + " bolsa(s).");
+        return temEstoque;
     }
-
-    @Override
-    public boolean validarDados() {
-        if (cargo == null || cargo.isEmpty()) {
-            return false;
-        }
-
-        if (matricula == null || matricula.isEmpty()) {
-            return false;
-        }
-        return true;
-    }
-
+    
     @Override
     public void mostrardados() {
-        System.out.println("Informações do administrador:");
-
+        System.out.println("== Informações do administrador ==");
         super.mostrarDados();
-
-        System.out.println("Cargo: " + this.cargo);
-        System.out.println("Matrícula: " + this.matricula);
     }
 }
