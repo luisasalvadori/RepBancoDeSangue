5f6e5b91197ba0bfd47dc5516a0ca29670e2725b
luisasalvadori <luisa.salvadori@gec.inatel.br>
2026-06-04 11:11:56 -0300

    Update Doador.java

--- RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Doador/Doador.java
+++ RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Doador/Doador.java
@@ -1,7 +1,10 @@
 package br.inatel.bancodesangue.Pessoa.Doador;
 
 import br.inatel.bancodesangue.Pessoa.Pessoa;
-
+import br.inatel.bancodesangue.Exceptions.DadosInvalidosException;
+import br.inatel.bancodesangue.Exceptions.TipoSanguineoException;
+import br.inatel.bancodesangue.Util.DataUtil;
+import br.inatel.bancodesangue.Util.ValidadorDados;
 import java.time.LocalDate;
 
 public class Doador extends Pessoa {
@@ -10,37 +13,39 @@ public class Doador extends Pessoa {
     protected boolean aptoDoacao;
     protected LocalDate ultimaDoacao;
     protected LocalDate proximaDoacao;
-    protected int bolsasDoacao;
+    protected int bolsasDoadas;
 
     public Doador(String nome, int idade, String cpf, String sexo, String tipoS, double peso, boolean aptoDoacao, LocalDate ultimaDoacao, LocalDate proximaDoacao, int bolsasDoacao) {
         super(nome, idade, cpf, sexo);
-        this.tipoS = tipoS;
+        ValidarDados.validarTipoSanguineo(tipoS);
+        ValidadorDados.validarDataNaoFutura(ultimaDoacao, "Última doação");
+        this.tipoS = tipoS.trim().toUpperCase();
         this.peso = peso;
-        this.aptoDoacao = validarDados();
         this.ultimaDoacao = ultimaDoacao;
         this.proximaDoacao = calcularProximaDoacao();
-        this.bolsasDoacao = bolsasDoacao;
+        this.bolsasDoadas = bolsasDoadas;
+        this.aptoDoacao = validarDados();
     }
 
     @Override
     public boolean validarDados(){
-        if(idade<16 || idade>69){
+        if(getIdade()<16 || getIdade()>69){
             return false;
         }
         if(peso<50){
             return false;
         }
-        if(idade>60 && bolsasDoacao == 0) {
+        if(getIdade()>60 && bolsasDoacao == 0) {
             return false;
         }
-        return true;
+        return LocalDate.now().isEqual(proximaDoacao) || LocalDate.now().isAfter(proximaDoacao);
     }
 
     public LocalDate calcularProximaDoacao(){
         if(ultimaDoacao == null){
             return LocalDate.now();
         }
-        if(sexo.equalsIgnoreCase("masculino")){
+        if(getSexo().equalsIgnoreCase("masculino")){
             return ultimaDoacao.plusDays(60);
         }
         else{
@@ -48,15 +53,31 @@ public class Doador extends Pessoa {
         }
     }
 
+    public void registrarDoacao() throws DadosInvalidosException {
+        ValidarDados.validarDataNaoFutura(dataColeta, "Data de coleta da doação");
+        bolsasDoadas++;
+        ultimaDoacao = dataColeta;
+        proximaDoacao = calcularProximaDoacao();
+        aptoDoacao = false;
+    }
+
     @Override
     public void mostrarDados(){
-        System.out.println("Informações do doador:");
+        System.out.println("===== DOADOR =====");
         super.mostrarDados();
-        System.out.println("Peso: " + this.peso);
-        System.out.println("Tipo sanguíneo: " + this.tipoS);
-        System.out.println("Última doação: " + this.ultimaDoacao);
-        System.out.println("Próxima doação: " + this.proximaDoacao);
-        System.out.println("Quantidade de bolsas doadas: " + this.bolsasDoacao);
-        System.out.println("Apto para doação: " + this.aptoDoacao);
+        System.out.println("Tipo sanguíneo: " + tipoS);
+        System.out.println("Peso: " + peso);
+        System.out.println("Última doação: " + DataUtil.formatar(ultimaDoacao));
+        System.out.println("Próxima doação: " + DataUtil.formatar(proximaDoacao));
+        System.out.println("Quantidade de bolsas doadas: " + bolsasDoadas);
+        System.out.println("Apto para doação: " + aptoDoacao ? "sim":"não");
+    }
+
+    public String getTipoS(){
+        return tipoS;
+    }
+
+    public boolean isAptoDoacao(){
+        return aptoDoacao;
     }
 }
