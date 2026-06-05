f01c3ed95444374bb656f960675c14959a487702
luisasalvadori <luisa.salvadori@gec.inatel.br>
2026-06-05 20:48:45 -0300

    Update Pessoa.java

--- RepBancoDeSangue1/RepBancoDeSangue1/RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Pessoa.java
+++ RepBancoDeSangue1/RepBancoDeSangue1/RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Pessoa.java
@@ -1,11 +1,14 @@
 package br.inatel.bancodesangue.Pessoa;
 
+//classe abstrata (representa uma pessoa no sistema)
 public abstract class Pessoa {
+    //atributos comuns aos tipos de pessoa (Funcionário, Paciente, Doador)
     private String nome;
     private int idade;
     private String cpf;
     private String sexo;
 
+    //construtor da classe
     public Pessoa(String nome, int idade, String cpf, String sexo) {
         this.nome = nome;
         this.idade = idade;
@@ -13,6 +16,7 @@ public abstract class Pessoa {
         this.sexo = sexo;
     }
 
+    //método que mostra as informações comuns 
     public void mostrarDados(){
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
@@ -20,8 +24,10 @@ public abstract class Pessoa {
         System.out.println("Sexo: " + sexo);
     };
 
+    //método abstrato que valida os dados da pessoa (é implementado nas subclasses)
     public abstract boolean validarDados();
 
+    //getters que permitem acessar os atributos de Pessoa
     public String getNome() {
         return nome;
     }
