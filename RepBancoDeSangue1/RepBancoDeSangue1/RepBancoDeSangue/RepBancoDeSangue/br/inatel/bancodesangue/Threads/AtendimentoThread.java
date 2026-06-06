26d749b09b07c61ceb26ae23c78f00248a811154
luisasalvadori <luisa.salvadori@gec.inatel.br>
2026-06-05 22:00:24 -0300

    Update AtendimentoThread.java

--- RepBancoDeSangue1/RepBancoDeSangue1/RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Threads/AtendimentoThread.java
+++ RepBancoDeSangue1/RepBancoDeSangue1/RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Threads/AtendimentoThread.java
@@ -4,11 +4,13 @@ import br.inatel.bancodesangue.Pessoa.Funcionario.Biomedico;
 import br.inatel.bancodesangue.Sangue.BolsaSangue;
 import br.inatel.bancodesangue.Sangue.Solicitacao.SolicitacaoSangue;
 
+//classe que simula um atendimento simultaneo realizado por um biomedico
 public class AtendimentoThread extends Thread {
-  private Biomedico biomedico;
-  private SolicitacaoSangue solicitacao;
-  private BolsaSangue bolsa;
+  private Biomedico biomedico; //biomedico reponsável pela analise da solicitação
+  private SolicitacaoSangue solicitacao; //solicitação de sangue que será analisada
+  private BolsaSangue bolsa; // bolsa de sangue associada à solicitação
 
+  //construtor da classe 
   public AtendimentoThread(String nomeThread, Biomedico biomedico, SolicitacaoSangue solicitacao, BolsaSangue bolsa) {
     super(nomeThread);
     this.biomedico = biomedico;
@@ -16,12 +18,13 @@ public class AtendimentoThread extends Thread {
     this.bolsa = bolsa;
   }
 
+  //metodo executado quando a thread é iniciada
   @Override
   public void run() {
-    System.out.println("[" + getName() + "] Atendimento iniciado.");
-    biomedico.setSolicitacao(solicitacao);
-    biomedico.setBolsaEmAnalise(bolsa);
-    biomedico.liberarBolsa();
-    System.out.println("[" + getName() + "] Atendimento finalizado.");
+    System.out.println("[" + getName() + "] Atendimento iniciado."); //informa o inicio do atendimento 
+    biomedico.setSolicitacao(solicitacao); //associa a solicitação ao biomedico
+    biomedico.setBolsaEmAnalise(bolsa); //define a bolsa a ser analisada
+    biomedico.liberarBolsa(); // analise e liberação da bolsa
+    System.out.println("[" + getName() + "] Atendimento finalizado."); //informa o termino do atendimento
   }
 }
