ffb3d6d9c3795d0c7294b347792ff614555ccd50
luisasalvadori <luisa.salvadori@gec.inatel.br>
2026-06-05 21:53:37 -0300

    Update Paciente.java

--- RepBancoDeSangue1/RepBancoDeSangue1/RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Paciente/Paciente.java
+++ RepBancoDeSangue1/RepBancoDeSangue1/RepBancoDeSangue/RepBancoDeSangue/br/inatel/bancodesangue/Pessoa/Paciente/Paciente.java
@@ -7,28 +7,31 @@ import br.inatel.bancodesangue.Util.ValidadorDados;
 
 import java.time.LocalDate;
 
+//paciente herda de pessoa
 public class Paciente extends Pessoa {
+    //atributos específicos de paciente
     private String tipoS;
     private int nivelUrgencia;
     private double quantSangue;
     private int bolsasRecebidas;
     private String diagnostico;
 
+    //construtor de paciente 
     public Paciente(String nome, int idade, String cpf, String sexo, String tipoS, int nivelUrgencia, double quantSangue, int bolsasRecebidas, String diagnostico) throws TipoSanguineoException {
-        super(nome, idade, cpf, sexo);
-        ValidadorDados.validarTipoSanguineo(tipoS);
-        this.tipoS = tipoS.trim().toUpperCase();
+        super(nome, idade, cpf, sexo); //chama o construtor da classe mãe
+        ValidadorDados.validarTipoSanguineo(tipoS); // valida se o tipo sanguíneo informado existe
+        this.tipoS = tipoS.trim().toUpperCase(); // remove espaços e padroniza em letras maiúsculas 
         this.nivelUrgencia = nivelUrgencia;
         this.quantSangue = quantSangue;
         this.bolsasRecebidas = bolsasRecebidas;
         this.diagnostico = diagnostico;
     }
 
-    public SolicitacaoSangue solicitarSangue(){
+    public SolicitacaoSangue solicitarSangue(){ //metodo para criar a solicitação de sangue para o paciente (criada com o próprio paciente e data atual)
         return new SolicitacaoSangue(this, LocalDate.now());
     }
 
-    public int calcularprioridade(){
+    public int calcularprioridade(){ //metodo para calcular a prioridade do paciente com base no nivel de urgencia
         if(nivelUrgencia >=8){
             return 1;
         }
@@ -41,20 +44,24 @@ public class Paciente extends Pessoa {
         //1-alta prioridade, 2-média prioridade, 3-baixa prioridade
     };
 
+    //método que registra a quantidade de bolsas recebidas
     public void receberBolsa(){
 
         bolsasRecebidas++;
     }
-    
+
+    //sobrescrita do método para verificar se os dados especificos estao corretos
     @Override
     public boolean validarDados() {
+        // idade deve ser maior que 0, tipo sanguíneo não pode ser vazio ou null, o nivel de urgencia deve ser maior ou igual a 0 e a quantidade de sangue deve ser maior que 0
         return getIdade()>0 && tipoS!=null && !tipoS.isBlank() && nivelUrgencia>=0 && nivelUrgencia<=10 && quantSangue>0;
     }
 
+    //sobrescrita do método que exibe as informações completas do paciente
     @Override
     public void mostrarDados(){
         System.out.println("===== PACIENTE =====");
-        super.mostrarDados();
+        super.mostrarDados(); //exibe os dados comuns herdados da superclasse 
         System.out.println("Tipo sanguíneo: " + tipoS);
         System.out.println("Diagnóstico: " + diagnostico);
         System.out.println("Nível de urgência: " + nivelUrgencia);
@@ -63,6 +70,7 @@ public class Paciente extends Pessoa {
         System.out.println("Bolsas recebidas: " + bolsasRecebidas);
     }
 
+    //getter
     public String getTipoS() {
         return tipoS;
     }
